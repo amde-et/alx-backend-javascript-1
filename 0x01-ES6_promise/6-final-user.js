159e79d6c5c96d91f32600c97fe3792540384b18
Candiepih <mutheeal.am@gmail.com>
2021-11-24 12:43:11 +0300

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(fileName);
   return Promise.allSettled([p1, p2]);
 }
-
-export default handleProfileSignup;
