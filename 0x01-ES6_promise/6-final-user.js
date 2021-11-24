a2d91362aa609ae9d8b60c1473586c9c505f44f7
Candiepih <mutheeal.am@gmail.com>
2021-11-24 13:17:10 +0300

    Fix for settling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+function handleProfileSignup(firstName, lastName, fileName) {
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(fileName);
-  return Promise.allSettled([p1, p2]);
+
+  return Promise.allSettled([p1, p2]).then((results) => {
+    const [user, photo] = results;
+    if (photo.status === 'rejected') {
+      photo.value = photo.reason.message;
+      delete photo.reason;
+    }
+    const data = [user, photo];
+    console.log(data);
+  });
 }
+
+export default handleProfileSignup;
