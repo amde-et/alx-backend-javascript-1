9d17114640004bdbd2b4e61c1913b18d63ac31d1
Candiepih <mutheeal.am@gmail.com>
2021-11-24 11:10:39 +0300

    modified file to eslint linting

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,6 @@
 export default function createIteratorObject(report) {
   const employees = Object.values(report.allEmployees);
-  const iter = employees.reduce((prev, current) => [...prev, ...current]);
-  return iter[Symbol.iterator]();
+  const employeesList = [];
+  employees.forEach((element) => employeesList.push(...element));
+  return employeesList;
 }
