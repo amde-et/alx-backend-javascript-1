6ddd493b076b51191f1ec577a99d5128aa77e2a4
Candiepih <mutheeal.am@gmail.com>
2021-11-24 12:25:15 +0300

    Formatted file to eslint linting

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
-function handleResponseFromAPI(promise) {
-  promise.then(
-      () => {
-        return {
-          status: 200,
-          body: 'success',
-        };
-      })
-    .catch(() => new Error())
+export default function handleResponseFromAPI(promise) {
+  return promise.then(
+      () => ({
+        status: 200,
+        body: 'success',
+      }),
+    ).catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
-
-export default handleResponseFromAPI;
