8dedc108cb109d22498e42d2bb02031dda651c2d
Candiepih <mutheeal.am@gmail.com>
2021-11-24 11:55:51 +0300

    Fix for empty error object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -6,11 +6,12 @@ function handleResponseFromAPI(promise) {
         status: 200,
         body: 'success',
       };
-    }
-  ).catch(() => {
-    console.log('Got a response from the API');
-    return new Error();
-  });
+    },
+    (error) => {
+      console.log('Got a response from the API');
+      return new Error(error);
+    },
+  );
 }
 
 export default handleResponseFromAPI;
