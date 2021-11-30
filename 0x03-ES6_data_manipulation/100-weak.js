cdb5017d6393e4b1ce636f86301bb43ba2aa3cfd
Candiepih <mutheeal.am@gmail.com>
2021-11-30 15:44:43 +0300

    Changed test for maximum queries

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export const queryAPI = (endpoint) => {
   if (weakMap.has(endpoint)) {
     const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 5) {
+    if (endpointData >= 4) {
       throw new Error('Too many requests');
     }
     weakMap.set(endpoint, (endpointData + 1));
