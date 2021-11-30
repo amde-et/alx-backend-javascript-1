4cc80886b2a7fbe8007e31f1d6b26720d8e3713d
Candiepih <mutheeal.am@gmail.com>
2021-11-30 15:34:11 +0300

    Added test for set values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,11 @@ const cleanSet = (set, startString) => {
   const strings = [];
 
   if (startString === '' || typeof startString !== 'string') return '';
-  for (const string of set) {
-    if (string.startsWith(startString)) {
-      strings.push(string.slice(startString.length));
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
     }
-  }
+  });
   return strings.join('-');
 };
 
