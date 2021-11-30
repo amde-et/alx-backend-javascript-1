032d30d5fd9ec41f9291f7e264dbf3bf829b16f8
Candiepih <mutheeal.am@gmail.com>
2021-11-30 15:46:32 +0300

    Changed message for maximum requests

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,8 @@ export const weakMap = new WeakMap();
 export const queryAPI = (endpoint) => {
   if (weakMap.has(endpoint)) {
     const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 4) {
-      throw new Error('Too many requests');
+    if (endpointData >= 5) {
+      throw new Error('Endpoint load is high');
     }
     weakMap.set(endpoint, (endpointData + 1));
   } else {
