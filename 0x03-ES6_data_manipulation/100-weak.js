9c4348e7c60f91a8857f253d880691d3f9fc93ec
Candiepih <mutheeal.am@gmail.com>
2021-11-30 15:42:56 +0300

    Reorganized if statements

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
 export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
-  const endpointData = weakMap.get(endpoint);
-  if (endpointData === undefined) {
-    weakMap.set(endpoint, 1);
-  } else {
+  if (weakMap.has(endpoint)) {
+    const endpointData = weakMap.get(endpoint);
     if (endpointData >= 5) {
       throw new Error('Too many requests');
     }
     weakMap.set(endpoint, (endpointData + 1));
+  } else {
+    weakMap.set(endpoint, 1);
   }
 };
