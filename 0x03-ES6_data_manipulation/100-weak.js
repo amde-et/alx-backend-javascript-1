5e609dee1ba728d2e38e495bef9fbc0a3d09fc95
Candiepih <mutheeal.am@gmail.com>
2021-11-30 15:37:59 +0300

    Modified test for requests number

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
   const endpointData = weakMap.get(endpoint);
-  if (endpointData >= 5) {
+  if (endpointData >= 4) {
     throw new Error('Too many requests');
   } else if (endpointData === undefined) {
     weakMap.set(endpoint, 1);
