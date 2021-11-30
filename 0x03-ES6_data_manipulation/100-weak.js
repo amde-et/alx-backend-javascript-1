dbf55f6a6019d75dcc70a12057cdf9f1469e47b1
Candiepih <mutheeal.am@gmail.com>
2021-11-30 15:47:55 +0300

    Modified error message

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export const queryAPI = (endpoint) => {
   if (weakMap.has(endpoint)) {
     const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 5) {
+    if (endpointData >= 4) {
       throw new Error('Endpoint load is high');
     }
     weakMap.set(endpoint, (endpointData + 1));
