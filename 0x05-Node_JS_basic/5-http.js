030ba68248f2c5570c82b4ea210f8babd1163c5e
Candiepih <mutheeal.am@gmail.com>
2022-01-25 13:36:17 +0300

    Modified path handling

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,27 +7,19 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 const fileName = process.argv[2];
 
-// Http paths
-const path = {
-  '/': (res) => {
+const app = http.createServer((req, res) => {
+  const {
+    url,
+  } = req;
+  if (url === '/') {
     res.end('Hello Holberton School!');
-  },
-  '/students': (res) => {
+  } else if (url === '/students') {
     countStudents(fileName).then((data) => {
       data.unshift('This is the list of our students');
       res.end(data.join('\n'));
     }).catch((error) => {
       res.end(`${error}`);
     });
-  },
-};
-
-const app = http.createServer((req, res) => {
-  const {
-    url,
-  } = req;
-  if (url in path) {
-    path[url](res);
   }
 });
 
