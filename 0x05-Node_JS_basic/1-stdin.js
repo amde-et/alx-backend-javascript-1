84ffee0ee077e6838d674f856c9e7c3c0e2a6020
Candiepih <44834632+candiepih@users.noreply.github.com>
2022-01-25 12:26:21 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,9 +4,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', (name) => {
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}`);
 });
 
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+process.stdin.on('exit', () => {
+  process.stdout.write('This important software is now closing');
 });
