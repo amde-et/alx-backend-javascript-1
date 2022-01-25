9c551f79ba0f06ea70a12ab00ed8bdf49be41774
Candiepih <44834632+candiepih@users.noreply.github.com>
2022-01-25 12:31:58 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,9 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  console.log(`Your name is: ${name}`);
+  if (name) {
+    console.log(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('exit', () => {
