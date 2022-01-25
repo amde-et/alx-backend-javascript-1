1e32828ff96ef19f49e3a5462fa05d9d007b30d3
Candiepih <mutheeal.am@gmail.com>
2022-01-25 14:41:09 +0300

    Handled content type

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,6 +8,7 @@ const port = 1245;
 const fileName = process.argv[2];
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
