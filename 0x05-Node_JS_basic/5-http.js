43dbc11c0e8fdda31e95c49778f1f0c8bafef98a
Candiepih <mutheeal.am@gmail.com>
2022-01-25 16:22:55 +0300

    Formatted file to eslint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,61 +1,8 @@
 /**
  * Create a small HTTP server using the http module.
  */
-const fs = require('fs');
 const http = require('http');
-
-/**
- * readData: reads the data and counts the
- * number of students in each field.
- * @param {list} data - list of students
- */
-const readData = (data) => {
-  const logs = [];
-  const studentsArray = data.trim().split('\n').slice(1);
-
-  logs.push('This is the list of our students');
-  const studentInfo = `Number of students: ${studentsArray.length}`;
-  logs.push(studentInfo);
-
-  const students = studentsArray.map((student) => {
-    const fields = student.replace('\r', '').split(',');
-    return fields;
-  });
-
-  const categories = [...new Set(students.map(
-    (student) => student[student.length - 1],
-  ))];
-
-  categories.forEach((category) => {
-    const filteredStudents = students.filter(
-      (student) => student[student.length - 1] === category,
-    ).map((student) => student[0]);
-
-    const info = `Number of students in ${category
-    }: ${filteredStudents.length}. List: ${filteredStudents.join(', ')}`;
-    logs.push(info);
-  });
-
-  return logs;
-};
-
-/**
- * countStudents - Reads the file asynchronously.
- * @param {string} database - path to database file
- * @returns promise
- */
-const countStudents = (database) => {
-  const readFilePromise = new Promise((resolve, reject) => {
-    fs.readFile(database, 'utf8', (error, data) => {
-      if (error) {
-        reject(Error('Cannot load the database'));
-      } else {
-        resolve(readData(data));
-      }
-    });
-  });
-  return readFilePromise;
-};
+const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 const fileName = process.argv[2];
@@ -65,9 +12,9 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(fileName).then((data) => {
+      res.setHeader('Content-Type', 'text/plain');
       res.end(data.join('\n'));
     }).catch((error) => {
-      res.statusCode = 500;
       res.end(`${error}`);
     });
   }
