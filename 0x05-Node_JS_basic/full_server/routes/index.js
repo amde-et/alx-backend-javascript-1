d3c911fe9d514cda83b271a87aba198eb3ee147f
Candiepih <mutheeal.am@gmail.com>
2022-01-25 11:11:10 +0300

    Modified controllers

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -5,12 +5,15 @@ const {
   Router,
 } = require('express');
 const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentsController');
+// const StudentsController = require('../controllers/StudentsController');
 
 const router = Router();
 
-router.get('/', AppController.getHomepage());
-router.get('/students', StudentsController.getStudents());
-router.get('/students/:major', StudentsController.getAllStudentsByMajor());
+router.get('/', (req, res) => {
+  // AppController.getHomepage(req, res);
+  res.status(200).send('Hello Holberton School!');
+});
+// router.get('/students', StudentsController.getStudents());
+// router.get('/students/:major', StudentsController.getAllStudentsByMajor());
 
 module.exports = router;
