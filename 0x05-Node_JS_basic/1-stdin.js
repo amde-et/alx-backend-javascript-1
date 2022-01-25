057a40108a05e1dfbd8b0bb3c603b814c355019b
Candiepih <44834632+candiepih@users.noreply.github.com>
2022-01-25 12:46:54 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
