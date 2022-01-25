46399afbea50aaa2ba84029d861ca50fb41c6c10
Candiepih <mutheeal.am@gmail.com>
2022-01-25 11:20:45 +0300

    Modified routes

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -5,12 +5,12 @@ const {
   Router,
 } = require('express');
 const AppController = require('../controllers/AppController');
-// const StudentsController = require('../controllers/StudentsController');
+const StudentsController = require('../controllers/StudentsController');
 
 const router = Router();
 
 router.get('/', AppController.getHomepage);
-// router.get('/students', StudentsController.getStudents());
-// router.get('/students/:major', StudentsController.getAllStudentsByMajor());
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 module.exports = router;
