ab823efbbfd2bb5e8250f56140164fc14b96b57a
Candiepih <mutheeal.am@gmail.com>
2022-01-26 11:51:09 +0300

    Formatted file to eslint

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,13 +1,14 @@
 /**
  * Contains all the functions that are used as students route handlers
  */
+const {
+  argv,
+} = require('process');
 const readDatabase = require('../utils');
-
-const fileName = process.argv[2];
 
 class StudentsController {
   static getAllStudents(_req, res) {
-    readDatabase(fileName).then((data) => {
+    readDatabase(argv[2]).then((data) => {
       const logs = ['This is the list of our students'];
       for (const [k, v] of Object.entries(data)) {
         logs.push(`Number of students in ${k}: ${v.length
@@ -25,9 +26,8 @@ class StudentsController {
     if (!acceptedMajors.includes(field)) {
       res.status(500).send('Major parameter must be CS or SWE');
     }
-    readDatabase(fileName).then((data) => {
-      const s = `List: ${data[field].join(', ')}`;
-      res.status(200).send(s);
+    readDatabase(argv[2]).then((data) => {
+      res.status(200).send(`List: ${data[field].join(', ')}`);
     }).catch(() => {
       res.status(500).send('Cannot load the database');
     });
