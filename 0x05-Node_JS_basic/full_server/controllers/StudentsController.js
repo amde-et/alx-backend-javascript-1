6c9d886c714434da8695345725f5a299c5371f83
Candiepih <mutheeal.am@gmail.com>
2022-01-26 11:39:23 +0300

    Fix /students/:major response

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,16 +6,16 @@ const readDatabase = require('../utils');
 const fileName = process.argv[2];
 
 class StudentsController {
-  static getAllStudents(request, response) {
+  static getAllStudents(_req, res) {
     readDatabase(fileName).then((data) => {
       const logs = ['This is the list of our students'];
       for (const [k, v] of Object.entries(data)) {
         logs.push(`Number of students in ${k}: ${v.length
         }. List: ${v.join(', ')}`);
       }
-      response.status(200).send(logs.join('\n'));
+      res.status(200).send(logs.join('\n'));
     }).catch(() => {
-      response.status(500).send('Cannot load the database');
+      res.status(500).send('Cannot load the database');
     });
   }
 
@@ -26,7 +26,7 @@ class StudentsController {
       res.status(500).send('Major parameter must be CS or SWE');
     }
     readDatabase(fileName).then((data) => {
-      res.status(200).send(`List: ${data[field].join(', ')}`);
+      res.status(200).send(`List: ${data[field].join(', ')}\n`);
     }).catch(() => {
       res.status(500).send('Cannot load the database');
     });
